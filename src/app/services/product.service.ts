d7fca5000a97a1df0d3d0886da5c66dbe8e96735
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2018-09-23 23:21:17 +0500

    added basic product service to fetch products from backend

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+
+import { Product } from '../classes/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  getProducts(): Observable<Product> {
+
+    const http_url = 'api/product';
+    const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+
+    return this.http.get<Product>(http_url, http_options);
+  }
+}
