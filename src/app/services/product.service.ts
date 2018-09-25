0036f2f0540df0135c251abd1df943d9fc60341c
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2018-09-25 07:32:27 +0500

    addProduct() method added to Product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,4 +22,14 @@ export class ProductService {
 
     return this.http.get<Product[]>(http_url, http_options);
   }
+
+  addProduct(product: Product) {
+    const http_url = 'api/product/add';
+    const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+
+    const data = JSON.stringify(product);
+ 
+    return this.http.post<Product>(http_url, data,  http_options);
+
+  }
 }
