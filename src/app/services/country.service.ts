f8561b3b916461d5aa7a48260b8265e6f65c0c71
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2019-01-05 05:43:30 +0500

    created getCountry(id), editCountry(id), deleteCountry() in CountryService.

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { ICountry } from '../interfaces/icountry';
 import { Country } from '../classes/country';
@@ -20,10 +20,28 @@ export class CountryService {
     return this.http.get<ICountry[]>(http_url, http_options);
   }
 
+  getCountry(id: String): Observable<Country> {
+    const http_url = 'api/country/' + id;
+    const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'}) };
+    return this.http.get<ICountry>(http_url, http_options);
+  }
+
   addCountry(country: Country): Observable<Country> {
     const http_url = 'api/country/add';
     const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'}) };
     return this.http.post<ICountry>(http_url, country, http_options);
+  }
+
+  editCountry(country: Country) {
+    const http_url = 'api/country/edit/' + country.id;
+    const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'}) };
+    return this.http.put<ICountry>(http_url, country, http_options);
+  }
+
+  deleteCountry(country: Country) {
+    const http_url = 'api/country/delete/' + country.id;
+    const http_options = { headers: new HttpHeaders({'Content-Type': 'application/json'}) };
+    return this.http.delete(http_url, http_options);
   }
 
 }
