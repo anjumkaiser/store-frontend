044171a61b70bba13bbdf8346869ed2cbe6acd69
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2019-05-19 14:31:32 +0500

    JWT is now verified against public key, and then token is set for authenication

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { KEYUTIL, KJUR } from 'jsrsasign';
 
 export class AuthUser {
   username: string;
@@ -18,7 +19,9 @@ class AuthResponse {
 export class AuthenticationService {
 
   private loggedIn = new BehaviorSubject<boolean>(false);
+  public publicKey: null | string = null;
 
+  public user: any = new BehaviorSubject<any>(null)
   constructor(
     private router: Router,
     private http: HttpClient,
@@ -28,10 +31,23 @@ export class AuthenticationService {
     } else {
       this.loggedIn.next(false);
     }
+
+    if (this.publicKey == null) {
+      this.getKey();
+    }
    }
 
   isLoggedIn(): BehaviorSubject<boolean> {
     return this.loggedIn;
+  }
+
+  private getKey() {
+    this.publicKey = null;
+    fetch('api/authenticate/jwtkey/pem').then(resp => {
+      resp.text().then(x => {
+        this.publicKey = x;
+      });
+    });
   }
 
 
@@ -39,8 +55,23 @@ export class AuthenticationService {
     const http_url = 'api/authenticate/password';
     const http_options = { headers: new HttpHeaders({'Accept': 'application/json'})};
     this.http.post<AuthResponse>(http_url, auth_user, http_options).subscribe((resp: AuthResponse) => {
-      localStorage.setItem('token', resp.token);
-      this.loggedIn.next(true);
+
+      if (!this.publicKey) {
+        return;
+      }
+
+      const pubkey = KEYUTIL.getKey(this.publicKey);
+      const isValid = KJUR.jws.JWS.verifyJWT(resp.token, pubkey, {
+        alg: ['RS256', 'RS512'],
+      });
+
+      if (isValid) {
+        const  parsedJWT = KJUR.jws.JWS.parse(resp.token);
+        this.user.next(parsedJWT.payloadObj.pvt.user);
+        localStorage.setItem('token', resp.token);
+        this.loggedIn.next(true);
+      }
+
     }, (e: HttpErrorResponse) => {
       localStorage.removeItem('token')
       this.loggedIn.next(false);
